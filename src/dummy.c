67fc151f32e8590a765eb0010ff3444c584bbeb9
Stephen Milborrow <milbo@sonic.net>
2011-03-25 00:00:00 +0000

    version 1.2-1

--- src/dummy.c
+++ src/dummy.c
@@ -3,9 +3,9 @@
 This file is needed because we need somewhere to store the tests
 directory (other than at the root of this package, because we do not
 want to run the tests automatically).  The tests directory is big and we
-don't want people to download it unneccesarily when they install the
+don't want people to download it unnecessarily when they install the
 package. It seems that the only way we can achieve that is to store the
-tests in the src directory.  But an empty src dirctory causes rcmd check
+tests in the src directory.  But an empty src directory causes rcmd check
 to fail.  Hence this dummy file.
 
 */
